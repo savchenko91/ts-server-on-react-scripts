d363f0c6d5a0baa67439c9abaf53a159149e9a96
savchenko alexander <savchenkoalexander91@gmail.com>
2021-01-01 00:33:32 +0400

    refactoring

--- scripts/start.js
+++ scripts/start.js
@@ -1,59 +1,45 @@
-'use strict';
+'use strict'
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'production';
-
-const { exec } = require('child_process');
+process.env.BABEL_ENV = 'development'
+process.env.NODE_ENV = 'production'
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
-});
+process.on('unhandledRejection', (err) => {
+  throw err
+})
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../config/env')
 
-const fs = require('fs');
-const chalk = require('react-dev-utils/chalk');
-const webpack = require('webpack');
-const clearConsole = require('react-dev-utils/clearConsole');
+const fs = require('fs')
+const chalk = require('react-dev-utils/chalk')
+const webpack = require('webpack')
+const clearConsole = require('react-dev-utils/clearConsole')
 
 let subprocess
 // TODO choose port
 
-const {
-  createCompiler,
-  prepareUrls,
-  choosePort,
-} = require('./webpackDevServerUtils');
+const { createCompiler, prepareUrls, choosePort } = require('./webpackDevServerUtils')
 
-const paths = require('../config/paths');
+const paths = require('../config/paths')
 
-const appName = require(paths.appPackageJson).name;
+const appName = require(paths.appPackageJson).name
 
-const useYarn = fs.existsSync(paths.yarnLockFile);
+const useYarn = fs.existsSync(paths.yarnLockFile)
 
 // Tools like Cloud9 rely on this.
-const DEFAULT_PORT = parseInt(process.env.PORT_SERVER, 10) || 3001;
-const HOST = process.env.HOST || '0.0.0.0';
+const DEFAULT_PORT = parseInt(process.env.PORT_SERVER, 10) || 3001
+const HOST = process.env.HOST || '0.0.0.0'
 
 if (process.env.HOST) {
   console.log(
-    chalk.cyan(
-      `Attempting to bind to HOST environment variable: ${chalk.yellow(
-        chalk.bold(process.env.HOST)
-      )}`
-    )
-  );
-  console.log(
-    `If this was unintentional, check that you haven't mistakenly set it in your shell.`
-  );
-  console.log(
-    `Learn more here: ${chalk.yellow('https://bit.ly/CRA-advanced-config')}`
-  );
-  console.log();
+    chalk.cyan(`Attempting to bind to HOST environment variable: ${chalk.yellow(chalk.bold(process.env.HOST))}`),
+  )
+  console.log(`If this was unintentional, check that you haven't mistakenly set it in your shell.`)
+  console.log(`Learn more here: ${chalk.yellow('https://bit.ly/CRA-advanced-config')}`)
+  console.log()
 }
 
 const devSocket = {
@@ -66,7 +52,6 @@ const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
 const useTypeScript = fs.existsSync(paths.appTsConfig)
 
 const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true'
-
 
 clearConsole()
 
@@ -82,7 +67,7 @@ async function run() {
   const urls = prepareUrls(protocol, HOST, port)
   process.env.PORT_SERVER = port
 
-  const configFactory = require('../config/webpack.config');
+  const configFactory = require('../config/webpack.config')
   const config = configFactory('development')
 
   setTimeout(() => {
@@ -103,10 +88,7 @@ async function run() {
         aggregateTimeout: 300,
         poll: undefined,
       },
-      (err, stats) => {
-      },
-      )
-  },)
+      (err, stats) => {},
+    )
+  })
 }
-
-
